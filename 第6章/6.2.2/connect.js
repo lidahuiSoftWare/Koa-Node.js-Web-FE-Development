ac3582d5df2f8bcca2c8ec6ec2a960298ff1dc55
biyou.lh <biyou.lh@alibaba-inc.com>
2020-06-25 12:03:35 +0800

    增加数据库连接

--- 第6章/6.2.2/connect.js
+++ 第6章/6.2.2/connect.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('databaseName', 'userName', 'password', {
+const sequelize = new Sequelize('nodesql', 'root', 'root1234', {
   host: 'localhost',
   dialect: 'mysql',
   operatorsAliases: false,
